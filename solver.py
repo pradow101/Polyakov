0ac1090fab544dfd63c25d1f8409f267df3dee4f
joao.prado <joao.prado>
2025-03-17 15:03:16 -0300

    3o commit

--- solver.py
+++ solver.py
@@ -2,7 +2,9 @@ from definicoes import *
 import matplotlib.pyplot as plt
 
 
-u = 0
+u = 0.2
 T = 0.2
-chuteinit = np.array([0.1,0.1,0.1])
+chuteinit = np.array([0.05,0.05,0.1])
+
+
 sistema(u,T,chuteinit)
